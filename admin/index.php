e242c19024d174885457f20822790b7caa5928e2
mickidum <mickidum@gmail.com>
2019-08-23 18:04:18 +0300

    cors disabled for production

--- admin/index.php
+++ admin/index.php
@@ -122,21 +122,21 @@ $app->get('/getform/{form}', function (Request $request, Response $response, arr
 });
 
 // CORS FOR DEVELOPMENT - COMMENT ON PRODUCTION!!!
-$app->options('/{routes:.+}', function ($request, $response, $args) {
-	return $response;
-});
+// $app->options('/{routes:.+}', function ($request, $response, $args) {
+// 	return $response;
+// });
 
-$app->add(function ($req, $res, $next) {
-	$response = $next($req, $res);
-	return $response
-		->withHeader('Access-Control-Allow-Origin', '*')
-		->withHeader('Access-Control-Allow-Headers', 'X-Requested-With, Content-Type, Accept, Origin, Authorization')
-		->withHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, PATCH, OPTIONS');
-});
+// $app->add(function ($req, $res, $next) {
+// 	$response = $next($req, $res);
+// 	return $response
+// 		->withHeader('Access-Control-Allow-Origin', '*')
+// 		->withHeader('Access-Control-Allow-Headers', 'X-Requested-With, Content-Type, Accept, Origin, Authorization')
+// 		->withHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, PATCH, OPTIONS');
+// });
 
-$app->map(['GET', 'POST', 'PUT', 'DELETE', 'PATCH'], '/{routes:.+}', function ($req, $res) {
-	$handler = $this->notFoundHandler; // handle using the default Slim page not found handler
-	return $handler($req, $res);
-});
+// $app->map(['GET', 'POST', 'PUT', 'DELETE', 'PATCH'], '/{routes:.+}', function ($req, $res) {
+// 	$handler = $this->notFoundHandler; // handle using the default Slim page not found handler
+// 	return $handler($req, $res);
+// });
 
 $app->run();
